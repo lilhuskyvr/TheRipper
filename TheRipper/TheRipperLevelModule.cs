f60482674db9fc2585c38d65fb8151cbd4715555
Lil Husky VR <lilhuskyvr@gmail.com>
2021-01-30 21:26:01 +1100

    version 1 done

---
+++ TheRipper/TheRipperLevelModule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using ThunderRoad;
+using UnityEngine;
+
+namespace TheRipper
+{
+    public class TheRipperLevelModule : LevelModule
+    {
+        public override IEnumerator OnLoadCoroutine(Level level)
+        {
+            EventManager.onCreatureSpawn += EventManagerOnonCreatureSpawn;
+            return base.OnLoadCoroutine(level);
+        }
+
+        private void EventManagerOnonCreatureSpawn(Creature creature)
+        {
+            foreach (var part in creature.ragdoll.parts)
+            {
+                foreach (var handle in part.handles)
+                {
+                    handle.Grabbed += HandleOnGrabbed;
+                    handle.UnGrabbed += HandleOnUnGrabbed;
+                }
+            }
+        }
+
+        private void HandleOnUnGrabbed(RagdollHand ragdollhand, Handle handle, EventTime eventtime)
+        {
+            try
+            {
+                var ragdollPart = handle.GetComponentInParent<RagdollPart>();
+                ragdollPart.ResetCharJointBreakForce();
+            }
+            catch (Exception exception)
+            {
+                Debug.Log(exception.Message);
+            }
+        }
+
+        private void HandleOnGrabbed(RagdollHand ragdollhand, Handle handle, EventTime eventtime)
+        {
+            try
+            {
+                var ragdollPart = handle.GetComponentInParent<RagdollPart>();
+                if (!ragdollPart.name.Contains("Spine"))
+                    ragdollPart.characterJoint.breakForce = 1500;
+            }
+            catch (Exception exception)
+            {
+                Debug.Log(exception.Message);
+            }
+        }
+    }
+}
